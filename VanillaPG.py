ea8496a6364e9365d41d5e22c2678bc165562b7d
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-05-19 20:15:41 +0300

    training visualization & test episodes

--- VanillaPG.py
+++ VanillaPG.py
@@ -1,10 +1,13 @@
 import gym
 import torch
-import torch.nn as nn
 import numpy as np
+import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 from PPOconfig import PPOconfig
 from PPOnetwork import Policy
+
+
 
 
 class Policy(nn.Module):
@@ -19,6 +22,8 @@ class Policy(nn.Module):
         return x
 
 
+
+
 env = gym.make('CartPole-v1')  # Replace 'CartPole-v1' with your desired environment
 input_size = env.observation_space.shape[0]
 output_size = env.action_space.n
@@ -26,7 +31,10 @@ output_size = env.action_space.n
 policy = Policy(input_size, output_size)
 
 
+
+
 def train(policy, num_episodes, gamma, optimizer):
+    episode_rewards = []
     for episode in range(num_episodes):
         state = env.reset()
         episode_reward = 0
@@ -63,14 +71,45 @@ def train(policy, num_episodes, gamma, optimizer):
         
         optimizer.step()
         
+        episode_rewards.append(episode_reward)
+        
         print(f"Episode {episode + 1}/{num_episodes}, Reward: {episode_reward}")
+    
+    plt.plot(range(1, num_episodes + 1), episode_rewards)
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.title('Training Progress')
+    plt.save()
 
 
+def visualize_episodes(policy, num_episodes):
+    rewards = []
+    for _ in range(num_episodes):
+        state = env.reset()
+        episode_reward = 0
+        while True:
+            action_probs = policy(torch.Tensor(state))
+            action = torch.argmax(action_probs).item()
+            next_state, reward, done, _ = env.step(action)
+            episode_reward += reward
+            state = next_state
+            if done:
+                break
+        rewards.append(episode_reward)
+
+
+
+
+# обучение на 500 эпизодах
 num_episodes = 500
 gamma = 0.99
 learning_rate = 0.01
-
 optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
-
-
 train(policy, num_episodes, gamma, optimizer)
+
+
+
+
+# визуализация 10 тестовых эпизодов 
+num_test_episodes = 10
+visualize_episodes(policy, num_test_episodes)
